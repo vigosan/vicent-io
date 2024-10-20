83734665c4036bfcf4976d92c8dc83f1b1c1b9b9
Vicent Gozalbes <vigosan@gmail.com>
2024-10-20 19:42:18 +0200

    Update main page

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="mx-auto flex min-h-screen w-full max-w-5xl flex-col gap-6 px-6 py-12 text-gray-500 antialiased lg:gap-24 lg:px-24 lg:py-16">
-      <header className="flex flex flex-col justify-between">
+      <header className="flex  flex-col justify-between">
         <Link href="/">
-          <h1 className="font-medium font-normal tracking-wide text-gray-800">
+          <h1 className="font-medium tracking-wide text-gray-800">
             Vicent Gozalbes
           </h1>
         </Link>
@@ -14,12 +14,12 @@ export default function Home() {
 
       <main className="flex flex-col gap-12 lg:gap-24">
         <article className="flex flex-col gap-6">
-          <h2 className="font-medium font-normal text-gray-800">Today</h2>
+          <h2 className="font-medium text-gray-800">Today</h2>
           <p>
             I work at{" "}
             <a
               href="https://www.flywire.com"
-              className="underline"
+              className="underline font-medium"
               target="_blank"
             >
               Flywire
@@ -30,12 +30,12 @@ export default function Home() {
         </article>
 
         <article className="flex flex-col gap-6">
-          <h2 className="font-medium font-normal text-gray-800">Previously</h2>
+          <h2 className="font-medium text-gray-800">Previously</h2>
           <p>
             As the Tech Lead at{" "}
             <a
               href="https://www.strands.com"
-              className="underline"
+              className="underline font-medium"
               target="_blank"
             >
               Strands
@@ -46,12 +46,26 @@ export default function Home() {
         </article>
 
         <article className="flex flex-col gap-6">
-          <h2 className="font-medium font-normal text-gray-800">More</h2>
+          <h2 className="font-medium text-gray-800">Projects</h2>
+          <div>
+            <a href="https://gotwifi.vicent.io" className="font-medium underline">
+              Gotwifi
+            </a>{" "}
+            is an application that enhances the WiFi sharing experience by using
+            QR codes. It allows you to easily share your WiFi connection with
+            family or guests, adding a personal touch with your favorite photos.
+            Forget about confusing network names and complicated passwords; just
+            scan and connect instantly.
+          </div>
+        </article>
+
+        <article className="flex flex-col gap-6">
+          <h2 className="font-medium text-gray-800">More</h2>
           <p>
-            You can check out my coding projects on{" "}
+            You can check out my code on{" "}
             <a
               href="https://github.com/vigosan"
-              className="underline"
+              className="underline font-medium"
               target="_blank"
             >
               GitHub
@@ -60,10 +74,18 @@ export default function Home() {
             sessions on{" "}
             <a
               href="https://www.twitch.tv/vigosan"
-              className="underline"
+              className="underline font-medium"
               target="_blank"
             >
               Twitch
+            </a>{" "}
+            or{" "}
+            <a
+              href="https://www.instagram.com/vigosan"
+              className="underline font-medium"
+              target="_blank"
+            >
+              Instagram
             </a>
             .
           </p>
